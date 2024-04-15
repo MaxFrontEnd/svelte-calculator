3c9db06b039535ea157fe66fa9a404911915bf92
MAXQA <maxidromlordus@gmail.com>
2024-04-15 13:44:05 +0300

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,160 @@
+<script lang="ts">
+	const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.'];
+	const operations = ['/', '*', '-', '+', '='];
+
+	let selectedOperation = '';
+	let display = '';
+	let firstNumber = '';
+	let secondNumber = '';
+	const handleOperationClick = (operation: string) => {
+		if (firstNumber === '') return;
+		if (operation === '=') {
+			if (secondNumber === '') return;
+			const firstNum = parseInt(firstNumber);
+			const secondNum = parseInt(secondNumber);
+
+			let results = '';
+			switch (selectedOperation) {
+				case '/':
+					results = (firstNum / secondNum).toFixed(2);
+					break;
+
+				case '*':
+					results = (firstNum * secondNum).toFixed(2);
+					break;
+
+				case '+':
+					results = (firstNum + secondNum).toFixed(2);
+					break;
+
+				case '-':
+					results = (firstNum - secondNum).toFixed(2);
+					break;
+			}
+
+			display = results;
+		}
+		selectedOperation = operation;
+	};
+
+	const handleNumberClick = (number: string) => {
+		if (display === '' && number === '0') return;
+		if (number === '.' && display.includes('.')) return;
+		if (display === '' && number === '.') return (display = '0.');
+
+		if (!selectedOperation) {
+			if (display === '' && number === '.') {
+				firstNumber = '0.';
+				display = firstNumber;
+			}
+			firstNumber = `${firstNumber}${number}`;
+			display = firstNumber;
+		} else {
+			display = '';
+			if (display === '' && number === '.') {
+				secondNumber = '0.';
+			}
+			secondNumber = `${secondNumber}${number}`;
+			display = secondNumber;
+		}
+	};
+	const clearResult = () => {
+		firstNumber = '';
+		secondNumber = '';
+		display = '';
+		selectedOperation = '';
+	};
+</script>
+
+<main>
+	<div class="calculator">
+		<div class="results">{display}</div>
+		<div class="digits">
+			<div class="numbers">
+				<button
+					class="btn btn-xlg"
+					on:click={() => {
+						clearResult();
+					}}>C</button
+				>
+				{#each numbers as number (number)}
+					<button
+						class={`btn ${number === '0' ? 'btn-lg' : null}`}
+						on:click={() => {
+							handleNumberClick(number);
+						}}>{number}</button
+					>
+				{/each}
+			</div>
+			<div class="operations">
+				{#each operations as operator (operator)}
+					<button
+						class={`btn ${operator === selectedOperation ? 'btn-silver' : 'btn-orange'}`}
+						on:click={() => {
+							handleOperationClick(operator);
+						}}>{operator}</button
+					>
+				{/each}
+			</div>
+		</div>
+	</div>
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+	.calculator {
+		background-color: rgb(28, 28, 28);
+		width: 240px;
+		padding: 15px;
+		border-radius: 7px;
+	}
+	.digits {
+		display: flex;
+	}
+	.operations {
+		display: flex;
+		flex-direction: column;
+	}
+	.numbers {
+		display: flex;
+		flex-wrap: wrap;
+		width: 200px;
+	}
+	.btn {
+		width: 50px;
+		height: 50px;
+		border-radius: 100px;
+		background-color: rgb(114, 113, 113);
+		font-size: 20px;
+		font-weight: bold;
+		color: white;
+		margin: 5px;
+		border: none;
+	}
+	.btn-lg {
+		width: 110px;
+	}
+	.btn-orange {
+		background-color: orange;
+	}
+	.btn-silver {
+		background-color: silver;
+	}
+	.btn-xlg {
+		width: 170px;
+	}
+	.results {
+		height: 60px;
+		color: white;
+		font-size: 50px;
+		display: flex;
+		flex-direction: row-reverse;
+		margin-right: 10px;
+	}
+</style>
